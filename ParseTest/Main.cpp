4c262136276bcae905c5c7eecdbd894ad82ae1f6
christoph2005 <christoph2005@gmail.com>
2019-04-19 14:40:48 -0700

    Reverted previous commit changing project config file

--- ParseTest/Main.cpp
+++ ParseTest/Main.cpp
@@ -5,19 +5,19 @@
 
 int main(int argc, char* argv[]) {
 
-	printf(">>>>>>>>>>>>>>>>>>>> Reading the XML File <<<<<<<<<<<<<<<<<<<<\n\n");
+	printf("|>>>>>>>>>>>>>>>>>>>> Reading the XML File <<<<<<<<<<<<<<<<<<<<\n\n");
 	new ReadTaskDescriptionsFromXML();
-	printf("\n>>>>>>>>>>>>>>>>>>>> Done Reading XML File <<<<<<<<<<<<<<<<<<<<\n");
+	printf("\n|>>>>>>>>>>>>>>>>>>>> Done Reading XML File <<<<<<<<<<<<<<<<<<<<\n");
 
 
-	printf(">>>>>>>>>>>>>>>>>>>> Scanning Directory for Files <<<<<<<<<<<<<<<<<<<<\n\n");
+	printf("|>>>>>>>>>>>>>>>>>>>> Scanning Directory for Files <<<<<<<<<<<<<<<<<<<<\n\n");
 	new ScanDirectoryForFilesTest("C:\\Users\\chris\\Documents");
-	printf("\n>>>>>>>>>>>>>>>>>>>> Done Scanning Directory for Files <<<<<<<<<<<<<<<<<<<<\n");
+	printf("\n|>>>>>>>>>>>>>>>>>>>> Done Scanning Directory for Files <<<<<<<<<<<<<<<<<<<<\n");
 
 
-	printf(">>>>>>>>>>>>>>>>>>>> Scanning Files for Dates <<<<<<<<<<<<<<<<<<<<\n\n");
+	printf("|>>>>>>>>>>>>>>>>>>>> Scanning Files for Dates <<<<<<<<<<<<<<<<<<<<\n\n");
 	new ReadFileDateTimes("C:\\Users\\chris\\Documents\\TempTitle.txt");
-	printf("\n>>>>>>>>>>>>>>>>>>>> Done Scanning Files for Dates <<<<<<<<<<<<<<<<<<<<\n");
+	printf("\n|>>>>>>>>>>>>>>>>>>>> Done Scanning Files for Dates <<<<<<<<<<<<<<<<<<<<\n");
 
 	return 0;
 }
